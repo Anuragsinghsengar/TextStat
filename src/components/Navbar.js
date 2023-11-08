bc4cd2f9ccf37c47fc8249384d8f054c146d4e58
Anuragsinghsengar <131814714+Anuragsinghsengar@users.noreply.github.com>
2023-11-08 16:25:43 +0530

    blue button margin corrected

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,7 @@ export default function Navbar(props) {
         </ul>
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn-outline-success mx-1" type="submit">Search</button>
       </form>
       <div className="p-3 mb-2 bg-primary text-white mx-1" onClick={props.BlueMode}></div>
       <div className="p-3 mb-2 bg-success text-white mx-1" onClick={props.GreenMode}></div> 
